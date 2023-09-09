5f448a141c5518e4c47caae14c25e72ccae6256c
Georg <im@gmge.org>
2023-09-09 16:04:00 -0400

    update nan and inf cases

--- torchlambertw/special.py
+++ torchlambertw/special.py
@@ -1,37 +1,132 @@
 """Module for implementing the special function, Lambert W.
 
 In special module to follow the style of scipy.special.* and tfp.special.*
+
+This implementation is a direct translation from TensorFlow Probability
+https://www.tensorflow.org/probability/api_docs/python/tfp/math/lambertw
 """
 
+from typing import Tuple
 import torch
+import numpy as np
+
+_EPS = torch.finfo(torch.float32).eps
+# Constant for - 1 / e.  This is the lowest 'z' for which principal / non-principal W
+# is real valued (W(-1/e) = -1).  For any z < -1 / exp(1), W(z) = NA.
+_EXP_INV = np.exp(-1)
+_M_EXP_INV = -1 * _EXP_INV
 
 
-def _init_lambertw(x: torch.Tensor) -> torch.Tensor:
-    z0 = torch.zeros_like(x)
-    z0[x > 1.0] = x[x > 1.0]
-    z0[x < -2.0] = x[x < -2.0].exp() * (1.0 - x[x < -2.0].exp())
-    pade = lambda x: x * (3.0 + 6.0 * x + x ** 2.0) / (3.0 + 9.0 * x + 5.0 * x ** 2)
-    z0[(x <= 1.0) & (x >= -2.0)] = pade(x[(x <= 1.0) & (x >= -2.0)].exp())
-    z0[z0 == 0.0] = 1e-6
-    return z0
-
-
-def lambertw(x: torch.Tensor, branch: int = 0) -> torch.Tensor:
+def _lambertw_winitzki_approx(z: torch.Tensor) -> torch.Tensor:
     """
-    Computes the Lambert function via Halley algorithm which converges cubically.
-    The initialization is performed with a local approximation.
+    Computes Winitzki approximation to Lambert W function at z >= -1/exp(1).
 
     Args:
-      x: input Tensor.
-      branch: 0 or -1; 0 uses the principal branch (default); -1 the non-principal branch.
+        z: Value for which W(z) should be computed. Expected z >= -1/exp(1).
+
+    Returns:
+        lambertw_winitzki_approx: Approximation for W(z) for z >= -1/exp(1).
     """
-    z = _init_lambertw(x)
-    eps = torch.finfo(x.dtype).eps
-    a = lambda w: (w * ((w + eps).log() + w - x)) / (1 + w)
-    b = lambda w: -1 / (w * (1 + w))
-    for i in range(4):
-        c = a(z)
-        z = torch.max(
-            z - c / (1 - 0.5 * c * b(z)), torch.tensor([eps], dtype=x.dtype)[:, None]
-        )
-    return z
+    log1pz = torch.log1p(z)
+    return log1pz * (1.0 - torch.log1p(log1pz) / (2.0 + log1pz))
+
+
+def _halley_iteration(
+    w: torch.Tensor, z: torch.Tensor, tol: float, iteration_count: int
+) -> Tuple[bool, torch.Tensor, torch.Tensor, torch.Tensor, int]:
+    """
+    Halley's method on root finding of w for the equation w * exp(w) = z.
+
+    Args:
+        w (torch.Tensor): Current value of w.
+        z (torch.Tensor): Value for which the root is being found.
+        tol (float): Tolerance for convergence.
+        iteration_count (int): Current iteration count.
+
+    Returns:
+        Tuple[bool, torch.Tensor, torch.Tensor, torch.Tensor, int]:
+            - bool: Whether the iteration should stop.
+            - torch.Tensor: Updated value of w.
+            - torch.Tensor: Input value z.
+            - torch.Tensor: Delta value.
+            - int: Updated iteration count.
+    """
+    f = w - z * torch.exp(-w)
+    delta = f / (w + 1.0 - 0.5 * (w + 2.0) * f / (w + 1.0))
+    w_next = w - delta
+    converged = torch.abs(delta) <= tol * torch.abs(w_next)
+    should_stop_next = torch.all(converged) or (iteration_count >= 100)
+    return should_stop_next, w_next, z, delta, iteration_count + 1
+
+
+def _fritsch_iteration(
+    w: torch.Tensor, z: torch.Tensor, tol: float, iteration_count: int
+) -> Tuple[bool, torch.Tensor, torch.Tensor, torch.Tensor, int]:
+    """
+    Root finding iteration for W(z) using Fritsch iteration.
+
+    Args:
+        w (torch.Tensor): Current value of w.
+        z (torch.Tensor): Value for which the root is being found.
+        tol (float): Tolerance for convergence.
+        iteration_count (int): Current iteration count.
+
+    Returns:
+        Tuple[bool, torch.Tensor, torch.Tensor, torch.Tensor, int]:
+            - bool: Whether the iteration should stop.
+            - torch.Tensor: Updated value of w.
+            - torch.Tensor: Input value z.
+            - torch.Tensor: Delta value.
+            - int: Updated iteration count.
+    """
+    zn = torch.log(torch.abs(z)) - torch.log(torch.abs(w)) - w
+    wp1 = w + 1.0
+    q = 2.0 * wp1 * (wp1 + 2.0 / 3.0 * zn)
+    q_minus_2zn = q - 2.0 * zn
+    error = zn / wp1 * (1.0 + zn / q_minus_2zn)
+    delta = torch.abs(error * w)
+    converged = delta <= tol
+    should_stop_next = torch.all(converged) or (iteration_count >= 100)
+    return should_stop_next, w * (1.0 + error), z, delta, iteration_count + 1
+
+
+def _lambertw_principal_branch_nonna(z):
+    """Computes principal branch of Lambert W function for input with nonna output."""
+    # check if z > -1 (vectorized)
+    w = torch.where(z >= _M_EXP_INV, _lambertw_winitzki_approx(z), z)
+    stop_condition = False
+    counter = 0
+    while not stop_condition:
+        counter += 1
+        stop_condition, w, z, _, _ = _halley_iteration(w, z, _EPS, counter)
+
+    # if z = _M_EXP_INV, return exactly -1.
+    return torch.where(torch.abs(z - _M_EXP_INV) < _EPS, -1 * torch.ones_like(z), w)
+
+
+def _lambertw_principal_branch(z: torch.Tensor) -> torch.Tensor:
+    """Computes principal branch for z.
+
+    For z < -1/exp(1), it returns nan; for z = inf, it returns inf.
+    """
+    w = torch.where(z >= _M_EXP_INV, _lambertw_principal_branch_nonna(z), torch.nan)
+    return torch.where(torch.isposinf(z), torch.inf, w)
+
+
+def _lambertw_nonprincipal_branch(z: torch.Tensor) -> torch.Tensor:
+    """Computes the non-principal branch of z."""
+    return _lambertw_principal_branch(z)
+
+
+def lambertw(z: torch.Tensor, branch: int = 0) -> torch.Tensor:
+    """Computes the Lambert W function of 'z' for principal (0) and non-principal (-1) branch.
+
+    Args:
+        z: Value for which W(z) should be computed. Expected z >= -1/exp(1).
+    """
+    if branch == 0:
+        return _lambertw_principal_branch(z)
+    elif branch == -1:
+        return _lambertw_nonprincipal_branch(z)
+    else:
+        raise NotImplementedError(f"branch={branch} is not implemented. Only 0 or -1.")
--- torchlambertw/tests/test_special.py
+++ torchlambertw/tests/test_special.py
@@ -4,29 +4,60 @@ import torch
 import numpy as np
 import scipy.special
 import pytest
-import parameterized
+import timeit
 
 from .. import special
 
-
-_W_IDENTITIES = [
+# https://en.wikipedia.org/wiki/Lambert_W_function#Special_values
+_LAMBERTW_SPECIAL_VALUES = [
     (0.0, 0.0),
-    (1, 0.0),
-    (np.exp(-1), 0),
+    (1, 0.5671433),  # omega constant
+    (special._M_EXP_INV, -1.0),
+    (special._M_EXP_INV - 0.00001, np.nan),
+    (-10, np.nan),
+    (2 * np.log(2), np.log(2)),
+    (np.exp(1), 1),
+    (np.inf, np.inf),
+    (-np.inf, np.nan),
 ]
 
 
+def _test_z():
+    return np.array([[0.0, 1, 2.0], [-1.0, 3, 4.0]])
+
+
 def test_specific_values():
-    for (z, w_z) in _W_IDENTITIES:
+    for (z, expected) in _LAMBERTW_SPECIAL_VALUES:
         torch_z = torch.tensor(z)
-        result = special.lambertw(torch_z).numpy()[0][0]
-        assert result == pytest.approx(w_z, 1e-6)
+        w = float(special.lambertw(torch_z).numpy())
+        print(z, expected)
+        if np.isnan(expected):
+            assert np.isnan(w)
+        else:
+            assert w == pytest.approx(expected, 1e-6)
 
 
 @pytest.mark.skip(reason="skip for now")
 def test_torch_equals_scipy():
-    data = np.array([[1, 2.0], [3, 4.0]])
+    data = _test_z()
     torch_data = torch.tensor(data)
     scipy_results = scipy.special.lambertw(data)
     torch_results = special.lambertw(torch_data)
     np.testing.assert_allclose(torch_results.numpy(), scipy_results.real)
+
+
+@pytest.mark.skip(reason="skip; only used for local testing on local machine")
+def test_speed():
+    x_data = torch.Tensor(_test_z())
+    # Define a function to run the Lambert W function and measure the time
+    def _run_lambertw():
+        _ = special.lambertw(x_data)
+
+    # Measure the execution time using timeit
+    execution_time = timeit.timeit(
+        _run_lambertw, number=100
+    )  # Adjust the number of runs as needed
+    print(execution_time)
+    assert (
+        execution_time < 0.05
+    ), f"Execution time exceeded 100ms: {execution_time:.4f} seconds"
