0ac0f009c321efbdfb2d60ac8903480728c5d8a0
Georg <im@gmge.org>
2023-09-09 18:45:22 -0400

    update lambertw distribution transform

--- torchlambertw/distributions.py
+++ torchlambertw/distributions.py
@@ -4,3 +4,77 @@ import torch
 
 # This is WIP.
 from . import special
+from . import transforms
+import torch.distributions as td
+
+
+class LambertWNormal(td.transformed_distribution.TransformedDistribution):
+    r"""
+    Creates a Lambert W x Normal distribution parameterized by
+    `loc`, `scale`, and `tailweight` where::
+
+        X ~ Normal(loc, scale)
+        U = (X - loc) / scale
+        Z = U * exp(tailweight / 2. * U^2)
+        Y = Z * scale + loc
+        Y ~ Lambert W x Normal(loc, scale, tailweight)
+
+    Example::
+
+        >>> # xdoctest: +IGNORE_WANT("non-deterministic")
+        >>> m = LogNormal(torch.tensor([0.0]), torch.tensor([1.0]))
+        >>> m.sample()  # log-normal distributed with mean=0 and stddev=1
+        tensor([ 0.1046])
+
+    Args:
+        loc (float or Tensor): mean of log of distribution
+        scale (float or Tensor): standard deviation of log of the distribution
+    """
+    arg_constraints = {
+        "loc": td.constraints.real,
+        "scale": td.constraints.positive,
+        "tailweight": td.constraints.greater_than_eq(0.0),
+    }
+    support = td.constraints.real
+    has_rsample = True
+
+    def __init__(self, loc, scale, tailweight, validate_args=None):
+        self.tailweight = tailweight
+        super().__init__(
+            base_distribution=td.Normal(
+                loc=loc, scale=scale, validate_args=validate_args
+            ),
+            transforms=transforms.LambertWTailTransform(
+                shift=loc, scale=scale, tailweight=tailweight
+            ),
+            validate_args=validate_args,
+        )
+
+    def expand(self, batch_shape, _instance=None):
+        new = self._get_checked_instance(LambertWNormal, _instance)
+        return super().expand(batch_shape, _instance=new)
+
+    @property
+    def loc(self):
+        return self.base_dist.loc
+
+    @property
+    def scale(self):
+        return self.base_dist.scale
+
+    @property
+    def mean(self):
+        loc_abs_times_tailweight = (self.base_dist.loc.abs() + 1) * (
+            1.0 - self.tailweight
+        )
+        return torch.where(loc_abs_times_tailweight > 0, self.base_dist.loc, torch.nan)
+
+    @property
+    def mode(self):
+        return self.base_dist.loc
+
+    @property
+    def variance(self):
+        z_variance = 1.0 / torch.pow(1 - 2 * self.tailweight, 1.5)
+        y_variance = z_variance * self.base_dist.scale.pow(2)
+        return torch.where(y_variance > 0, y_variance, torch.inf)
--- torchlambertw/transforms.py
+++ torchlambertw/transforms.py
@@ -6,11 +6,12 @@ Lambert W transforms and Lambert W x F distributions come in 2 flavors:
 
 * heavy-tailed (potentially skewed) Lambert W transforms / distributions ('delta' & 'alpha') -- see Goerg (2015).
 
-Transform names follow original convention in `LambertW` R package (https://github.com/gmgeorg/LambertW).
+Transform names follow TensorFlow probability naming convention.
 """
 
 
 import torch
+import torch.distributions as td
 from . import special
 
 _EPS = torch.finfo(torch.float32).eps
@@ -19,6 +20,11 @@ _EPS = torch.finfo(torch.float32).eps
 def H_gamma(u: torch.tensor, gamma: torch.tensor) -> torch.tensor:
     """Computes base transform for skewed Lambert W x F distributions (Goerg 2011)."""
     return u * torch.tensor(gamma * u)
+
+
+def W_gamma(z: torch.tensor, gamma: torch.tensor, k: int) -> torch.tensor:
+    """Computes W_gamma(z), the inverse of H_gamma(u)."""
+    return special.lambertw(gamma * z, k=k) / gamma
 
 
 def G_delta(u: torch.tensor, delta: torch.tensor) -> torch.tensor:
@@ -33,11 +39,6 @@ def G_delta_alpha(
     return u * torch.exp(delta / 2.0 * ((u ** 2.0) ** alpha))
 
 
-def W_gamma(z: torch.tensor, gamma: torch.tensor, k: int) -> torch.tensor:
-    """Computes W_gamma(z), the inverse of H_gamma(u)."""
-    return special.lambertw(gamma * z, k=k) / gamma
-
-
 def W_delta(z: torch.tensor, delta: torch.tensor) -> torch.tensor:
     """Computes W_delta(z), the inverse of G_delta(u)."""
     delta_z2 = delta * z * z
@@ -46,3 +47,45 @@ def W_delta(z: torch.tensor, delta: torch.tensor) -> torch.tensor:
         z,
         torch.sqrt(special.lambertw(delta_z2, k=0) / delta) * torch.sign(z),
     )
+
+
+# distribution transforms
+
+
+class LambertWTailTransform(td.transforms.Transform):
+    r"""
+    Transform via the mapping :math:`y = x * \exp(delta / 2 * x**2)`.
+    """
+    domain = td.constraints.real
+    codomain = td.constraints.real
+    bijective = True
+    sign = +1
+
+    def __init__(
+        self,
+        shift: torch.tensor,
+        scale: torch.tensor,
+        tailweight: torch.tensor,
+        **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.shift = shift
+        self.scale = scale
+        self.tailweight = tailweight
+
+    def __eq__(self, other):
+        return isinstance(other, LambertWTailTransform)
+
+    def _call(self, x):
+        u = (x - self.shift) / self.scale
+        return G_delta(u, delta=self.tailweight) * self.scale + self.shift
+
+    def _inverse(self, y):
+        z = (y - self.shift) / self.scale
+        return W_delta(z, delta=self.tailweight) * self.scale + self.shift
+
+    # TODO:
+    def log_abs_det_jacobian(self, x, y):
+        raise NotImplementedError(
+            "Jacobian has not been implemented yet for LambertWTailTransform()."
+        )
